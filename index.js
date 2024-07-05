7f08439dcf4d776d4c6ad41000870ff71b81719c
Sharan-0-dot <sharansc482@gmail.com>
2024-07-05 10:28:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function onclick(){
+    const input=document.getElementById("input").value;
+    if(input=="car"){
+    let img1=innerHTML(`<img src="Ringbrothers-Dodge-Charger.jpg">`);
+    }
+    };
